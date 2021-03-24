8bfec2261b599a733e2480134c931c372c1bdd31
kthatoto <kthatoto@gmail.com>
2021-03-25 00:04:42 +0900

    Remove reset css

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -4,13 +4,4 @@ div
 </template>
 
 <style lang="stylus">
-*
-  margin: 0
-  padding: 0
-  box-sizing: border-box
-  list-style: none
-  text-decoration: none
-  user-select: none
-  &:visited
-    color: inherit
 </style>
