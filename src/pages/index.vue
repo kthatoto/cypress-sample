c75067ea0e31b0e6a7a916a2daca11fca8ff0fe1
kthatoto <kthatoto@gmail.com>
2021-03-25 00:04:18 +0900

    Add todo feature

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,81 @@
 <template lang="pug">
 .container
-  h1 Cypress Sample
+  h1 ToDo application
+  el-input(v-model="newTaskName")
+  el-button(type="primary" @click="createTask") 作成
+
+  ul
+    li(v-for="task in tasks" :key="task.id")
+      span
+        el-checkbox(v-model="task.done")
+        span {{ task.name }}
+      el-button(v-if="task.done" round) 削除
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, reactive, toRefs } from '@vue/composition-api'
+
+interface Task {
+  id: number
+  name: string
+  done: boolean
+}
 
 export default defineComponent({
   setup() {
+    const state = reactive<{
+      tasks: Task[]
+      newTaskName: string
+    }>({
+      tasks: [],
+      newTaskName: ""
+    })
+
+    const createTask = () => {
+      if (!state.newTaskName) return
+      const maxId = Math.max(...state.tasks.map(task => task.id))
+      state.tasks.push({
+        id: maxId + 1,
+        name: state.newTaskName,
+        done: false,
+      })
+    }
+
+    const doneTask = (id: number) => {
+      const targetIndex = state.tasks.findIndex(task => task.id === id)
+      if (targetIndex < 0) return
+      state.tasks.splice(targetIndex, 1, {
+        ...state.tasks[targetIndex],
+        done: true
+      })
+    }
+
+    const undoneTask = (id: number) => {
+      const targetIndex = state.tasks.findIndex(task => task.id === id)
+      if (targetIndex < 0) return
+      state.tasks.splice(targetIndex, 1, {
+        ...state.tasks[targetIndex],
+        done: false
+      })
+    }
+
+    const deleteTask = (id: number) => {
+      const targetIndex = state.tasks.findIndex(task => task.id === id)
+      if (targetIndex < 0) return
+      state.tasks.splice(targetIndex, 1)
+    }
+
+    const deleteDoneTasks = () => {
+      state.tasks = state.tasks.filter((task: Task) => !task.done)
+    }
+
+    return {
+      ...toRefs(state),
+      createTask,
+      doneTask,
+      deleteTask,
+      deleteDoneTasks,
+    }
   }
 })
 </script>
